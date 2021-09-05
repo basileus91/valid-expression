2fe92e8296ff1601afdc228ca70238871705c47a
Vasile Furdui <vasiafurdui@yahoo.com>
2021-09-05 19:33:33 +0300

    create app

---
+++ ASSUMPTIONS.md
@@ -0,0 +1,19 @@
+1. For this task I will create an angular application
+2. Created first task with **ng new valid-expression**
+3. I will use an input to write the expression
+4. Create 4 additional functios: 
+    4.1. **isOperator** to check if it's a valid operator
+    4.2. **isNumber** to check if it's a valid number
+    4.3. **isSpecialExpression** to check if it's a trigonometric value
+    4.4. **calculateString** to calculate string mathematical expression
+5. First I will divide this expression into array
+6. In case I have trigonometric expressions I will covert then to number
+7. Replace the trigonometric expression in the array with number
+8. From changed array I will convert back into string expression
+9. I will calculate mathematical string expression
+10. I will display under the input if the expression is valid
+
+Notes: 
+1. I used in build Math
+2. In this exercise app I will do the first 2 tasks.
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'valid-expression';
+  expression: any;
+  isValidExpression = false;
+  operatorExpression = new RegExp('(?<=[-+*/])|(?=[-+*/])');
+  sinExpression = new RegExp('sin(.+?)');
+  cosExpression = new RegExp('cos(.+?)');
+  tanExpression = new RegExp('tan(.+?)');
+  result = 0;
+
+  validate(): void {
+    const arr = this.expression.split(this.operatorExpression);
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < arr.length; i++) {
+      if (this.isOperator(arr[i].trim()) || this.isNumber(arr[i].trim()) || this.isSpecialExpression(arr[i])) {
+        this.isValidExpression = true;
+        if (this.isSpecialExpression(arr[i])){
+          if (this.sinExpression.test(arr[i].trim())) {
+            arr[i] = Math.sin(arr[i].match(/(\d+)/)[0]);
+          } else if (this.cosExpression.test(arr[i].trim())) {
+            arr[i] = Math.cos(arr[i].match(/(\d+)/)[0]);
+          } else if (this.tanExpression.test(arr[i].trim())) {
+            arr[i] =  Math.tan(arr[i].match(/(\d+)/)[0]);
+          }
+        }
+      } else {
+        this.isValidExpression = false;
+      }
+    }
+    // tslint:disable-next-line:variable-name
+    let string = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < arr.length; i++) {
+      string += arr[i];
+    }
+    this.result = this.calculateString(string);
+  }
+
+  calculateString(fn): number {
+    return new Function('return ' + fn)();
+  }
+
+  isOperator(str: string): boolean {
+    return this.operatorExpression.test(str);
+  }
+
+  isNumber(str: string): boolean {
+    return !isNaN(+str);
+  }
+
+  isSpecialExpression(str: string): boolean {
+    return this.sinExpression.test(str.trim()) || this.cosExpression.test(str.trim()) || this.tanExpression.test(str.trim());
+  }
 }
